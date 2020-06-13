b165ffb13fb6ea11a53b0a7ae9549a08e30fbffe
Stanislav Lapshin <staslapshin88@gmail.com>
2020-06-13 12:22:28 +0300

    [#] Improve conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,44 +1,21 @@
 # -*- coding: utf-8 -*-
 
-import os.path
-import sys
-
 from django.conf import settings
-
-sys.path.insert(0, os.path.dirname(__file__))
 
 
 def pytest_configure(config):
-    if not settings.configured:
-        test_db = os.environ.get("DB", "sqlite")
-
-        DATABASES = {"default": {"NAME": "bitfield"}}
-
-        if test_db == "postgres":
-            DATABASES["default"].update(
-                {
-                    "ENGINE": "django.db.backends.postgresql",
-                    "USER": "postgres",
-                },
-            )
-        elif test_db == "mysql":
-            DATABASES["default"].update(
-                {"ENGINE": "django.db.backends.mysql", "USER": "root"},
-            )
-        elif test_db == "sqlite":
-            DATABASES["default"].update(
-                {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"},
-            )
-        else:
-            raise NotImplementedError
-
-        settings.configure(
-            DATABASES=DATABASES,
-            INSTALLED_APPS=[
-                "django.contrib.contenttypes",
-                "jnt_django_toolbox",
-                "tests",
-            ],
-            ROOT_URLCONF="",
-            DEBUG=False,
-        )
+    settings.configure(
+        DATABASES={
+            "default": {
+                "ENGINE": "django.db.backends.sqlite3",
+                "NAME": ":memory:",
+            },
+        },
+        INSTALLED_APPS=[
+            "django.contrib.contenttypes",
+            "jnt_django_toolbox",
+            "tests",
+        ],
+        ROOT_URLCONF="",
+        DEBUG=False,
+    )
