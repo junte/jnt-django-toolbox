bf7f1cc2ed8618a41fd3b74114ca61703359dc84
Stanislav Lapshin <staslapshin88@gmail.com>
2020-06-29 18:47:03 +0300

    [#] Improve load_module_from_app

--- src/jnt_django_toolbox/helpers/modules.py
+++ src/jnt_django_toolbox/helpers/modules.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
 
 import importlib
+from contextlib import suppress
 
 
 def load_module_from_app(app_config, dotted_path: str):
     """Load module from application."""
     module_name = "{0}.{1}".format(app_config.name, dotted_path)
-    module_spec = importlib.util.find_spec(module_name)  # type: ignore
+    module_spec = None
+
+    with suppress(ModuleNotFoundError):
+        module_spec = importlib.util.find_spec(module_name)  # type: ignore
 
     if module_spec:
         return importlib.import_module(module_name)
