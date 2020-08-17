8a0e7e25271876a2e11d149623a46fe3550a60ac
Andrey Surovov <andrey.surovov@gmail.com>
2020-08-17 10:35:33 +0300

    [#2] implement "jnt_django_toolbox", updated version, fix linters.

--- tests/test_helpers/test_model/test_ensure_fields_saved.py
+++ tests/test_helpers/test_model/test_ensure_fields_saved.py
@@ -1,7 +1,10 @@
+# -*- coding: utf-8 -*-
+
 from jnt_django_toolbox.helpers.model import ensure_fields_saved
 
 
 def test_ensure_fields_save():
+    """Test func without update_fields."""
     kwargs = {"update_fields": ["created"]}
 
     ensure_fields_saved(kwargs, ["title"])
@@ -10,6 +13,7 @@ def test_ensure_fields_save():
 
 
 def test_ensure_fields_save_empty():
+    """Test func with update_fields."""
     kwargs = {}
 
     ensure_fields_saved(kwargs, ["title"])
