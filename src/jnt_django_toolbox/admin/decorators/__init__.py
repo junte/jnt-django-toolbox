f795caa998a4fde7f686414a5b87181665dfc942
Andrey Surovov <andrey.surovov@gmail.com>
2020-10-07 13:06:11 +0000

    Resolve "Implement decorator for actions in admin"

--- src/jnt_django_toolbox/admin/decorators/__init__.py
+++ src/jnt_django_toolbox/admin/decorators/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from .admin_action import admin_action
 from .admin_changelist_link import admin_changelist_link
 from .admin_field import admin_field
 from .admin_link import admin_link
