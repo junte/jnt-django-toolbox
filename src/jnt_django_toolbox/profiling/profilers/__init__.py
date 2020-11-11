52dbdf3e7fbf087f985c19c1f2a307a0433c53cc
Viacheslav Riazanov <riazanovslv@gmail.com>
2020-11-11 09:38:26 +0300

    Add cache manager stats to request #12

--- src/jnt_django_toolbox/profiling/profilers/__init__.py
+++ src/jnt_django_toolbox/profiling/profilers/__init__.py
@@ -1,6 +1,7 @@
 from contextlib import suppress
 
 from .db_queries import DatabaseQueriesProfiler
+from .cache_calls import CacheCallsProfiler
 
 with suppress(ImportError):
     from .jaeger import JaegerProfiler
--- src/jnt_django_toolbox/profiling/profilers/base.py
+++ src/jnt_django_toolbox/profiling/profilers/base.py
@@ -1,13 +1,16 @@
 from abc import ABC, abstractmethod
+from contextlib import ExitStack
+
+from django.http import HttpRequest, HttpResponse
 
 
 class BaseProfiler(ABC):
     """Base class for custom profilers."""
 
     @abstractmethod
-    def before_request(self, request, stack):
+    def before_request(self, request: HttpRequest, stack: ExitStack):
         """Call on recieve request."""
 
     @abstractmethod
-    def after_request(self, request, response):
+    def after_request(self, request: HttpRequest, response: HttpResponse):
         """Call after is processed and response available."""
