07c1b93e5f3e8523a06635c7d8ad99bcfa80da90
Andrey Surovov <andrey.surovov@gmail.com>
2021-02-03 12:28:56 +0300

    Add SQCount, update version: 0.1.50 -> 0.1.51 #17

--- src/jnt_django_toolbox/models/query.py
+++ src/jnt_django_toolbox/models/query.py
@@ -1,8 +1,9 @@
-from django.contrib.postgres.fields import ArrayField
 from django.db import models
-from django.db.models import Aggregate, Func, IntegerField, Subquery, Value
+from django.db.models import Subquery
 
 
 class SQCount(Subquery):
+    """Count items of subquery."""
+
     template = "(SELECT count(*) FROM (%(subquery)s) _count)"  # noqa: WPS323
     output_field = models.IntegerField()
