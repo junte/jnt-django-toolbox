8bbeb3c71f8a338fe404b9fe25e3df51477d1700
Stanislav Lapshin <sm.lapshin@gmail.com>
2022-07-05 18:03:34 +0300

    refactor(version): 0.4.2

--- src/jnt_django_toolbox/admin/helpers/urls.py
+++ src/jnt_django_toolbox/admin/helpers/urls.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.urls import NoReverseMatch, reverse
 
 
@@ -11,17 +12,19 @@ class _AdminUrlProvider:
         "autocomplete",
     )
 
-    def for_view(self, url_type, model):
+    def for_view(
+        self,
+        url_type: str,
+        model,
+        model_admin: admin.ModelAdmin | None = None,
+    ) -> str | None:
         if url_type not in self.allowed_urls_types:
             raise "Type not allowed, allowed: {0}".format(
                 self.allowed_urls_types,
             )
 
-        if not model:
-            return None
-
         model_id = getattr(model, "id", None)
-        view_name = self._make_view_name(url_type, model)
+        view_name = self._make_view_name(url_type, model, model_admin)
         return self._reverse(
             view_name,
             args=[model_id]
@@ -32,20 +35,40 @@ class _AdminUrlProvider:
     def add_url(self, model):
         return self.for_view("add", model)
 
-    def change_url(self, model):
-        return self.for_view("change", model)
+    def change_url(
+        self,
+        model,
+        model_admin: admin.ModelAdmin | None = None,
+    ) -> str | None:
+        return self.for_view("change", model, model_admin)
 
-    def delete_url(self, model):
-        return self.for_view("delete", model)
+    def delete_url(
+        self,
+        model,
+        model_admin: admin.ModelAdmin | None = None,
+    ) -> str | None:
+        return self.for_view("delete", model, model_admin)
 
-    def view_url(self, model):
-        return self.for_view("view", model)
+    def view_url(
+        self,
+        model,
+        model_admin: admin.ModelAdmin | None = None,
+    ) -> str | None:
+        return self.for_view("view", model, model_admin)
 
-    def list_url(self, model):
-        return self.for_view("changelist", model)
+    def list_url(
+        self,
+        model,
+        model_admin: admin.ModelAdmin | None = None,
+    ) -> str | None:
+        return self.for_view("changelist", model, model_admin)
 
-    def autocomplete_url(self, model):
-        return self.for_view("autocomplete", model)
+    def autocomplete_url(
+        self,
+        model,
+        model_admin: admin.ModelAdmin | None = None,
+    ) -> str | None:
+        return self.for_view("autocomplete", model, model_admin)
 
     def __call__(self, model):
         return {
@@ -53,14 +76,25 @@ class _AdminUrlProvider:
             for url_type in self.allowed_urls_types
         }
 
-    def _make_view_name(self, url_type, model):
-        return "admin:{0}_{1}_{2}".format(
+    def _make_view_name(
+        self,
+        url_type: str,
+        model,
+        model_admin: admin.ModelAdmin | None = None,
+    ) -> str:
+        if model_admin:
+            site = model_admin.admin_site
+        else:
+            site = admin.site
+
+        return "{0}:{1}_{2}_{3}".format(
+            site.name,
             model._meta.app_label,
             model._meta.model_name,
             url_type,
         )
 
-    def _reverse(self, view_name, **kwargs):
+    def _reverse(self, view_name: str, **kwargs) -> str | None:
         try:
             return reverse(view_name, **kwargs)
         except NoReverseMatch:
