6995b54878d4fbf80eb789d4b33f1808c6c2e7ba
Viacheslav Riazanov <riazanovslv@gmail.com>
2022-11-07 16:35:46 +0300

    refactor(libs): update django to 4.1

--- src/jnt_django_toolbox/static/jnt_django_toolbox/js/widgets/autocomplete.js
+++ src/jnt_django_toolbox/static/jnt_django_toolbox/js/widgets/autocomplete.js
@@ -1,62 +1,50 @@
-"use strict";
+'use strict';
 {
-    const $ = django.jQuery;
-    const init = function($element, options) {
-        const settings = $.extend({
-            ajax: {
-                data: function(params) {
-                    // data-autocomplete-<param>
-                    // example: data-autocomplete-id
+  const $ = django.jQuery;
 
-                    let queryParams = {
-                        term: params.term,
-                        page: params.page,
-                        app_label: $element.data("app-label"),
-                        model_name: $element.data("model-name"),
-                        field_name: $element.data("field-name"),
-                    }
-                    let prefix = "autocomplete-";
+  $.fn.djangoAdminSelect2 = function () {
+    $.each(this, function (i, element) {
+      $(element).select2({
+        ajax: {
+          data: (params) => {
+            let queryParams = {
+              term: params.term,
+              page: params.page,
+              app_label: element.dataset.appLabel,
+              model_name: element.dataset.modelName,
+              field_name: element.dataset.fieldName
+            }
+            let prefix = "autocomplete-";
 
-                    for (let fieldName in $element.data()) {
-                      if (fieldName.startsWith(prefix)){
-                         queryParams[fieldName.slice(prefix.length).toLowerCase()] = $element.data(fieldName);
-                      }
-                    }
-
-                    return queryParams;
-                }
-            },
-            escapeMarkup: function (text) {
-                return text;
-            },
-            templateSelection: function (data, container) {
-              if (data.hasOwnProperty("__badge__") && !data.text.startsWith("<a") && container && container.is("li")) {
-                data.text = data.__badge__;
+            for (let fieldName in $(element).data()) {
+              if (fieldName.startsWith(prefix)) {
+                queryParams[fieldName.slice(prefix.length).toLowerCase()] = $(element).data(fieldName);
               }
-              return data.text;
-            },
-        }, options);
-        $element.select2(settings);
-    };
+            }
 
-    $.fn.djangoAdminSelect2 = function(options) {
-        const settings = $.extend({}, options);
-        $.each(this, function(i, element) {
-            const $element = $(element);
-            init($element, settings);
-        });
-        return this;
-    };
-
-    $(function() {
-        // Initialize all autocomplete widgets except the one in the template
-        // form used when a new formset is added.
-        $('.admin-autocomplete').not('[name*=__prefix__]').djangoAdminSelect2();
+            return queryParams;
+          }
+        }, escapeMarkup: function (text) {
+          return text;
+        },
+        templateSelection: function (data, container) {
+          if (data.hasOwnProperty("__badge__") && !data.text.startsWith("<a") && container && container.is("li")) {
+            data.text = data.__badge__;
+          }
+          return data.text;
+        },
+      });
     });
+    return this;
+  };
 
-    $(document).on('formset:added', (function() {
-        return function(event, $newFormset) {
-            return $newFormset.find('.admin-autocomplete').djangoAdminSelect2();
-        };
-    })(this));
+  $(function () {
+    // Initialize all autocomplete widgets except the one in the template
+    // form used when a new formset is added.
+    $('.admin-autocomplete').not('[name*=__prefix__]').djangoAdminSelect2();
+  });
+
+  document.addEventListener('formset:added', (event) => {
+    $(event.target).find('.admin-autocomplete').djangoAdminSelect2();
+  });
 }
